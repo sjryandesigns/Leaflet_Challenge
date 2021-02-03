12ae7e0bec383fbf4bab6a55c5cce32672d101ac
sjryandesigns <sjryandesigns@gmail.com>
2021-02-03 14:04:19 -0800

    functions for color and radius

--- Leaflet_Step_1/static/js/logic.js
+++ Leaflet_Step_1/static/js/logic.js
@@ -21,7 +21,30 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
+function markerSize(magnitude){
+    return magnitude * 5;
+};
 
+function markerColor(depth){
+    if (depth > 90) {
+        return '#FF0000';
+    }
+    else if (depth >= 70) {
+        return '#FF6600';
+    }
+    else if (depth >= 50) {
+        return '#FFCC00';
+    }
+    else if (depth >= 50) {
+        return '#CCFF00';
+    }
+    else if (depth >= 50) {
+        return '#66FF00';
+    }
+    else {
+        return '#00FF00';
+    }
+};
 
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
@@ -32,58 +55,56 @@ d3.json(queryUrl).then(function(data) {
 
     var earthquake_markers = []
 
-    function markerSize(magnitude){
-        return magnitude * 5;
-    };
+
 
     earthquakes.forEach(function(earthquake){
         earthquake_markers.push(
             L.circle(earthquake.coordinates, {
                 stroke: false,
                 fillOpacity: 0.75,
-                color:"red",
-                fillColor: "red",
+                color: getColor(earthquake.coordinates[2]),
+                fillColor: getColor(earthquake.coordinates[2]),
                 radius:markerSize(earthquake.properties.mag) 
-            })
+            }) 
         )
-    }).addTo(myMap);
+    }).bindPopup("<h1>" + earthquake.properties.place + "</h1> <hr> <h3>" + new Date(earthquake.properties.time) + "<h3> <hr> <h3>" + earthquake.properties.mag + "</h3>").addTo(myMap);
     
 
 
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+    });
+
+    var earthquakes_l = L.layerGroup(earthquake_markers);
+
+    var baseMaps = {
+    "Street Map": streetmap,
+    };
+
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+        "Earthquakes": earthquakes
+    };
+
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+     var myMap = L.map("mapid", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [streetmap, earthquakes_l]
+    });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+
 });
-//     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//     tileSize: 512,
-//     maxZoom: 18,
-//     zoomOffset: -1,
-//     id: "mapbox/streets-v11",
-//     accessToken: API_KEY
-//     });.addTo(myMap);
-
-//   var baseMaps = {
-//     "Street Map": streetmap,
-//   };
-
-//   // Create overlay object to hold our overlay layer
-//   var overlayMaps = {
-//     Earthquakes: earthquakes
-//   };
-
-//   // Create our map, giving it the streetmap and earthquakes layers to display on load
-//   var myMap = L.map("mapid", {
-//     center: [
-//       37.09, -95.71
-//     ],
-//     zoom: 5,
-//     layers: [streetmap, earthquakes]
-//   });
-
-//   // Create a layer control
-//   // Pass in our baseMaps and overlayMaps
-//   // Add the layer control to the map
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(myMap);
-
 
 
