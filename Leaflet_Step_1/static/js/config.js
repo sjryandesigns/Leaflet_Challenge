3264032684587b0fd03f979a758da88e9d1e5b75
sjryandesigns <sjryandesigns@gmail.com>
2021-02-03 12:49:29 -0800

    initial map

--- Leaflet_Step_1/static/js/config.js
+++ Leaflet_Step_1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1Ijoic2pyeWFuZGVzaWducyIsImEiOiJja2s5dnZmcXUwMWI0MzFzMWJxejZveWd5In0.8x6OPbhudV3XwGWexV9ClA";
+const API_KEY = "pk.eyJ1Ijoic2pyeWFuZGVzaWducyIsImEiOiJja2twdzh5ZncwdjFzMm9udjRxZGtlNGQ4In0.LmHWolRoMsYYrlP1biCgKw";
---
+++ Leaflet_Step_1/static/js/logic.js
@@ -0,0 +1,51 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(data) {
+    // The data.features object is in the GeoJSON standard
+    console.log(data.features);
+
+    var earthquakes = L.geoJSON(data.features);
+
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  });
+
+  var baseMaps = {
+    "Street Map": streetmap,
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("mapid", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+
+
+
+
+
+});
