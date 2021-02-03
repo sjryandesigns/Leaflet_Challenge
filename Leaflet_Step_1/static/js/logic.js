1ba7056b143c434a6b2eca937e4cfa8aa6133fd4
sjryandesigns <sjryandesigns@gmail.com>
2021-02-03 14:38:00 -0800

    isolate functions, create map function

--- Leaflet_Step_1/static/js/logic.js
+++ Leaflet_Step_1/static/js/logic.js
@@ -41,6 +41,12 @@ d3.json(queryUrl).then(function(data) {
         pointToLayer: addMarker
     });
 
+    createMap(earthquakes);
+
+});
+
+
+
         // Add tile layer    
     var basemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
