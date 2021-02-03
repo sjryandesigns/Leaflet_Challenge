891d89db2329eee3b8137d744c6e0c7249d241ec
sjryandesigns <sjryandesigns@gmail.com>
2021-02-03 14:36:53 -0800

    rearrange code

--- Leaflet_Step_1/static/js/logic.js
+++ Leaflet_Step_1/static/js/logic.js
@@ -1,23 +1,3 @@
-
-
-// Create initial map object
-var myMap = L.map("mapid", {
-    center: [
-      37.09, -95.71],
-    zoom: 5,
-    });
-
-// Add tile layer    
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
-
-
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
@@ -51,10 +31,58 @@ function markerColor(depth){
     }
 };
 
+
+
 // Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
-    // The data.features object is in the GeoJSON standard
-    console.log(data.features);
+
+    var earthquakes = L.geoJSON(data.features,{
+        onEachFeature: addPopup,
+        pointToLayer: addMarker
+    });
+
+        // Add tile layer    
+    var basemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    });
+
+    var baseMaps = {
+        "Street Map": basemap,
+        };
+    
+        // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+        "Earthquakes": earthquakes
+    };
+    
+    // Create initial map object
+    var myMap = L.map("mapid", {
+        center: [
+        37.09, -95.71],
+        zoom: 5,
+        layers: [basemap, earthquakes]
+    });
+
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+      }).addTo(myMap);
+    
+    });
+
+
+
+
+
+
+
+
+
+
 
     var earthquakes = L.geoJSON(data.features);
 
@@ -76,25 +104,8 @@ d3.json(queryUrl).then(function(data) {
     
 
 
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-    });
-
     var earthquakes_l = L.layerGroup(earthquake_markers);
 
-    var baseMaps = {
-    "Street Map": streetmap,
-    };
-
-    // Create overlay object to hold our overlay layer
-    var overlayMaps = {
-        "Earthquakes": earthquakes
-    };
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
      var myMap = L.map("mapid", {
