85baf93c4f4402b5a8c38315b5a8345793811c35
sjryandesigns <sjryandesigns@gmail.com>
2021-02-03 14:07:10 -0800

    legend start

--- Leaflet_Step_1/static/js/logic.js
+++ Leaflet_Step_1/static/js/logic.js
@@ -105,6 +105,35 @@ d3.json(queryUrl).then(function(data) {
         collapsed: false
     }).addTo(myMap);
 
+
+    // Set up the legend
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = geojson.options.limits;
+    var colors = geojson.options.colors;
+    var labels = [];
+
+    // Add min & max
+    var legendInfo = "<h1>Median Income</h1>" +
+      "<div class=\"labels\">" +
+        "<div class=\"min\">" + limits[0] + "</div>" +
+        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "</div>";
+
+    div.innerHTML = legendInfo;
+
+    limits.forEach(function(limit, index) {
+      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Adding legend to the map
+  legend.addTo(myMap);
+
 });
 
 
